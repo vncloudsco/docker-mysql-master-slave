24a4dabb4280712eeafb944a8fdab2bda3088459
Mạnh Tường Solo <contact@manhtuong.net>
2025-09-16 11:35:43 +0700

    Update database user credentials in db-config.php for WordPress service

--- hyperdb/db-config.php
+++ hyperdb/db-config.php
@@ -1,16 +1,16 @@
 <?php
 $wpdb->add_database(array(
     'host'     => 'mysql_master',
-    'user'     => 'mydb_user',
-    'password' => 'mydb_pwd',
+    'user'     => 'root',
+    'password' => '111',
     'name'     => 'mydb',
     'write'    => 1,
     'read'     => 0,
 ));
 $wpdb->add_database(array(
     'host'     => 'mysql_slave',
-    'user'     => 'mydb_user',
-    'password' => 'mydb_pwd',
+    'user'     => 'root',
+    'password' => '111',
     'name'     => 'mydb',
     'write'    => 0,
     'read'     => 1,
