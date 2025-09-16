91a6697c8653d03c5918b01118114dd4b2776e83
Mạnh Tường Solo <contact@manhtuong.net>
2025-09-16 17:12:23 +0700

    Refactor database configuration to use environment variables for master and slave hosts

--- hyperdb/db-config.php
+++ hyperdb/db-config.php
@@ -3,18 +3,18 @@ $wpdb->save_queries = true;
 // $wpdb->persistent = true;
 // $wpdb->check_tcp_responsiveness = true;
 $wpdb->add_database(array(
-    'host'     => 'mysql_master',
-    'user'     => 'root',
-    'password' => '111',
-    'name'     => 'mydb',
+    'host'     => getenv_docker('WORDPRESS_DB_HOST_MASTER', 'mysql_master'),
+    'user'     => getenv_docker('WORDPRESS_DB_USER', 'root'),
+    'password' => getenv_docker('WORDPRESS_DB_PASSWORD', '111'),
+    'name'     => getenv_docker('WORDPRESS_DB_NAME', 'mydb'),
     'write'    => 1,
     'read'     => 1000,
 ));
 $wpdb->add_database(array(
-    'host'     => 'mysql_slave',
-    'user'     => 'root',
-    'password' => '111',
-    'name'     => 'mydb',
+    'host'     => getenv_docker('WORDPRESS_DB_HOST_SLAVE', 'mysql_slave'),
+    'user'     => getenv_docker('WORDPRESS_DB_USER', 'root'),
+    'password' => getenv_docker('WORDPRESS_DB_PASSWORD', '111'),
+    'name'     => getenv_docker('WORDPRESS_DB_NAME', 'mydb'),
     'write'    => 0,
     'read'     => 1,
 ));
